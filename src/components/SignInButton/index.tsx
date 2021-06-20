9925006c59d22a6e109fbed8aa1a1c0b8e4fec62
ridneto <ridineu@hotmail.com>
2021-06-20 16:41:50 -0300

    autenticaçao com o github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [ session ] = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session);
+
+    return session ? (
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => { signOut()} }
         >
             <FaGithub color="#04d361"/>
-            Ridineu de Moraes Neto
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     )   : (
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sign in with GitHub
